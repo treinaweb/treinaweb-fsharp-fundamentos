b40390a9e618b064821c13a7f734f4ebdfad75e0
Cleber Campomori <cleber.campomori@treinaweb.com.br>
2018-04-09 23:20:40 -0300

    Aula 3.1: Criando listas imutáveis com interação com o usuário com list comprehension

--- Program.fs
+++ Program.fs
@@ -2,21 +2,16 @@
 
 open System
 
-let buildMessage (name: string): string = 
-    if name.Length > 5 then
-        "Olá, " + name + ". Seu nome é longo."
-    else
-        "Olá, " + name + ". Seu nome é curto."
-
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    printfn "Digite seu nome: "
-    let mutable nome: string = Console.ReadLine()
-    // printf "Seu nome é %s\n" nome
-    // nome <- nome + "Mudei o valor"
-    // printf "Seu nome é %s\n" nome
-    // let nomeEhIgual = nome = "Mudei o valor"
-    // printf "%b\n" nomeEhIgual
-    printf "%s\n" (buildMessage nome)
+    let mutable name = ""
+    printf "Digite o nome de seus amigos. Digite 'sair' quando quiser interromper. \n"
+    let names = [
+        while name <> "sair" do
+            name <- Console.ReadLine()
+            if name <> "sair" then
+                yield name
+    ]
+    printf "Você tem %d amigos! \n" names.Length
+    printf "==============================\n"
     0 // return an integer exit code
