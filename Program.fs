88d1cedb5d76fa744c341a7bfbc66a26fb3fda74
Cleber Campomori <cleber.campomori@treinaweb.com.br>
2018-04-09 23:15:59 -0300

    Aula 2.6: Definindo funções no F#

--- Program.fs
+++ Program.fs
@@ -2,14 +2,21 @@
 
 open System
 
+let buildMessage (name: string): string = 
+    if name.Length > 5 then
+        "Olá, " + name + ". Seu nome é longo."
+    else
+        "Olá, " + name + ". Seu nome é curto."
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
     printfn "Digite seu nome: "
     let mutable nome: string = Console.ReadLine()
-    printf "Seu nome é %s\n" nome
-    nome <- nome + "Mudei o valor"
-    printf "Seu nome é %s\n" nome
+    // printf "Seu nome é %s\n" nome
+    // nome <- nome + "Mudei o valor"
+    // printf "Seu nome é %s\n" nome
     // let nomeEhIgual = nome = "Mudei o valor"
     // printf "%b\n" nomeEhIgual
+    printf "%s\n" (buildMessage nome)
     0 // return an integer exit code
