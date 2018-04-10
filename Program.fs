bf7f63db1c2a39f83fe604165127e602e6c3b2b7
Cleber Campomori <cleber.campomori@treinaweb.com.br>
2018-04-09 21:53:50 -0300

    Aula 2.3: Aspectos funcionais no F# - imutabilidade

--- Program.fs
+++ Program.fs
@@ -8,4 +8,8 @@ let main argv =
     printfn "Digite seu nome: "
     let nome = Console.ReadLine()
     printf "Seu nome é %s\n" nome
+    // nome = "Mudei o valor"
+    // printf "Seu nome é %s\n" nome
+    let nomeEhIgual = nome = "Mudei o valor"
+    printf "%b\n" nomeEhIgual
     0 // return an integer exit code
