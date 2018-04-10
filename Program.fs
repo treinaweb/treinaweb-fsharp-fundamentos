548bb084d5ea9f51c93155888a62ac9ac5b8c459
Cleber Campomori <cleber.campomori@treinaweb.com.br>
2018-04-09 21:49:49 -0300

    Aula 2.2: Declaração de 'variáveis' no F#

--- Program.fs
+++ Program.fs
@@ -5,4 +5,7 @@ open System
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    printfn "Digite seu nome: "
+    let nome = Console.ReadLine()
+    printf "Seu nome é %s\n" nome
     0 // return an integer exit code
