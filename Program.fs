fa3afc9b36f1bd2c1faade753255107df79ba200
Cleber Campomori <cleber.campomori@treinaweb.com.br>
2018-04-09 23:30:16 -0300

    Aula 4.1: Criando tipos estruturados no F# - types

--- Program.fs
+++ Program.fs
@@ -2,22 +2,31 @@
 
 open System
 
+type Person = {
+    name: string
+    age: int
+}
+
 [<EntryPoint>]
 let main argv =
     let mutable name = ""
     printf "Digite o nome de seus amigos. Digite 'sair' quando quiser interromper. \n"
     let names = [
         while name <> "sair" do
+            printf "Nome: "
             name <- Console.ReadLine()
             if name <> "sair" then
-                yield name
+                printf "Idade: "
+                let age = int(Console.ReadLine())
+                printf "******* \n"
+                yield { name = name; age = age }
     ]
     printf "Você tem %d amigos! \n" names.Length
     printf "==============================\n"
     // printf "Nomes dos amigos digitados: \n"
     // names |> List.iter(fun n -> printf " - %s \n" n)
-    names |> List.map(fun n -> "Olá, " + n)
+    names |> List.map(fun n -> "Olá, " + n.name)
           |> List.iter(fun welcome -> printf "%s \n" welcome)
     printf "==============================\n"
-    names |> List.iter(fun n -> printf " - %s \n" n)
+    // names |> List.iter(fun n -> printf " - %s \n" n)
     0 // return an integer exit code
