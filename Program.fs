daee569658b2c13a6e96ade24cecaf5453fb8d2d
Cleber Campomori <cleber.campomori@treinaweb.com.br>
2018-04-09 23:38:00 -0300

    Aula 5.1: Definindo funções com mais de um parâmetro

--- Program.fs
+++ Program.fs
@@ -13,6 +13,12 @@ let getOldestPerson people =
 let getYoungerPerson people =
     people |> List.minBy(fun p -> p.age)
 
+let getPageableContent people pageNumber = 
+    people |> List.sortBy(fun p -> p.name)
+           |> List.skip((pageNumber - 1) * 3)
+        //    |> List.take(3)
+           |> List.truncate(3)
+
 [<EntryPoint>]
 let main argv =
     let mutable name = ""
@@ -50,5 +56,14 @@ let main argv =
     let youngerPerson = names |> getYoungerPerson
     printf " - Seu amigo mais novo é o %s, com %d anos \n" youngerPerson.name youngerPerson.age
     printf " - A média de idade dos seus amigos é %d anos\n" ((names |> List.sumBy(fun person -> person.age)) / names.Length)
+    printf "==============================\n"
+
+    let mutable pageNumber = 1
+    while pageNumber <> 0 do
+        printf "Digite a página desejada: "
+        pageNumber <- int(Console.ReadLine())
+        if pageNumber <> 0 then
+            getPageableContent names pageNumber |> List.iter(fun p -> printf " - %s, %d anos \n" p.name p.age)
+            printf "==============================\n"
     // names |> List.iter(fun n -> printf " - %s \n" n)
     0 // return an integer exit code
